d491d57a20702da661ea8f1f2ea9ad81e5c5843e
Ryuji Masukawa <masukawa@nii.ac.jp>
2013-05-20 14:56:56 +0900

    install.inc.phpを元に戻す。

--- app/Config/install.inc.php
+++ app/Config/install.inc.php
@@ -2,12 +2,12 @@
 define("NC_DATASOURCE", 'Database/Mysql');
 define("NC_PERSISTENT", false);
 define("NC_HOST", 'localhost');
-define("NC_LOGIN", 'root');
-define("NC_PASSWORD", 'mysql');
-define("NC_DATABASE", 'masukawa_nc3');
+define("NC_LOGIN", '');
+define("NC_PASSWORD", '');
+define("NC_DATABASE", '');
 define("NC_PREFIX", 'nc3_');
 define("NC_ENCODING", 'utf8');
-Configure::write('Security.salt', '2c513d985439ec28e0dc1f95e352b99d5fb5622c');
-Configure::write('Security.cipherSeed', '2135448167212875942');
+Configure::write('Security.salt', '');
+Configure::write('Security.cipherSeed', '');
 
-define("NC_INSTALLED", true);
+define("NC_INSTALLED", false);
