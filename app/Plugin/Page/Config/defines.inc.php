e2aa1e77b63700b206f7ade95bb312347cd3539f
Ryuji Masukawa <masukawa@nii.ac.jp>
2013-07-12 13:32:58 +0900

    参加者修正:表示件数の調整。

--- app/Plugin/Page/Config/defines.inc.php
+++ app/Plugin/Page/Config/defines.inc.php
@@ -3,7 +3,7 @@
 define("PAGES_COMMUNITY_LIMIT", 10);	// 1ページに表示するページ数	TODO:Configでデフォルトの表示件数を設定できるほうがよい。
 define("PAGES_COMMUNITY_VIEWS", 5);		// ページ送り：リストを5ページ分まで表示
 define("PAGES_COMMUNITY_LIMIT_SELECT", "10|20|30|40|50|100");
-define("PAGES_PARTICIPANT_LIMIT_DEFAULT", "15");
+define("PAGES_PARTICIPANT_LIMIT_DEFAULT", "10");
 define("PAGES_PARTICIPANT_LIMIT_SELECT", "[10, 15, 20, 30, 50, 100]");
 
 define("PAGES_OPERATION_TIME_LIMIT", 2400);		// ペースト、ショートカット作成、移動時のタイムアウト時間(s)
