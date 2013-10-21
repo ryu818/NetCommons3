edf533ef1545aae167c792065becc57d1a201d5e
topaz2 <topaz2.3333@gmail.com>
2013-10-21 11:30:42 +0900

    Added package php5-xdebug

--- cookbooks/omusubi/attributes/default.rb
+++ cookbooks/omusubi/attributes/default.rb
@@ -13,4 +13,4 @@ default['versions']['postgresql'] = '9.1*'
 default['versions']['curl'] = '7.22.0*'
 default['versions']['imagemagick'] = '8:6.6.9.7*'
 default['versions']['php5-imagick'] = '3.1.0*'
-
+default['versions']['php5-xdebug'] = '2.1.0*'
--- cookbooks/omusubi/recipes/default.rb
+++ cookbooks/omusubi/recipes/default.rb
@@ -2,7 +2,11 @@ execute "apt-get" do
   command "apt-get update"
 end
 
-packages = %w{git subversion nginx php5 php5-mysql  php5-pgsql php5-curl php5-cli php5-fpm php-pear mysql-server postgresql curl imagemagick php5-imagick}
+packages = %w{
+  php5 php5-mysql php5-pgsql php5-curl php5-cli php5-fpm php5-imagick php5-xdebug php-pear
+  git subversion nginx
+  mysql-server postgresql curl imagemagick
+}
 
 packages.each do |pkg|
   package pkg do
