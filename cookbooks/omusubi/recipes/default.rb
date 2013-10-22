978b97b52fa432cfbec0e35d2e59a75ef5a7ea8c
topaz2 <topaz2.3333@gmail.com>
2013-10-22 14:20:18 +0900

    Upgraded middleware version for cakephp 3.0 requirements

--- cookbooks/omusubi/recipes/default.rb
+++ cookbooks/omusubi/recipes/default.rb
@@ -1,9 +1,14 @@
 # Configure your sources.list to the nearby mirror
-execute "sources.list-update" do
+execute "sources.list update" do
   command "sed -i 's/us.archive/ja.archive/g' /etc/apt/sources.list"
 end
 
-execute "apt-get" do
+execute "add latest php5 repository" do
+  command "apt-get remove php5*; add-apt-repository ppa:ondrej/php5"
+  not_if { ::File.exists?("/etc/apt/sources.list.d/ondrej-php5-#{node[:lsb][:codename]}.list")}
+end
+
+execute "apt-get update" do
   command "apt-get update"
 end
 
@@ -11,12 +16,15 @@ packages = %w{
   php5 php5-mysql php5-pgsql php5-curl php5-cli php5-fpm php5-imagick php5-xdebug php-pear
   git subversion nginx
   mysql-server postgresql curl imagemagick
+  lsb-core
 }
 
 packages.each do |pkg|
   package pkg do
     action [:install, :upgrade]
-    version node.default[:versions][pkg]
+    if node.default[:versions][pkg].kind_of? String
+      version node.default[:versions][pkg]
+    end
   end
 end
 
@@ -33,6 +41,12 @@ end
 template "/etc/nginx/conf.d/php-fpm.conf" do
   mode 0644
   source "php-fpm.conf.erb"
+end
+
+template "/etc/php5/fpm/php.ini" do
+  source "php.ini.erb"
+  mode 0644
+  variables(:directives => node[:php][:directives])
 end
 
 service 'apache2' do
