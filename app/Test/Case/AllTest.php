4c3fdd313d9162f92b3be167587fb5678ff8f7fb
NEKOGET <info@nekoget.net>
2013-10-25 12:10:59 +0900

    区切り文字を/ではなく定数に変更

--- app/Test/Case/AllTest.php
+++ app/Test/Case/AllTest.php
@@ -2,6 +2,7 @@
 class AllTest extends CakeTestSuite {
 	public static function suite() {
 		$suite = new CakeTestSuite('All tests');
+
 		//app/Testのテストを実行する
 		$suite->addTestDirectory(TESTS . 'Case' . DS . 'Controller');
 		$suite->addTestDirectory(TESTS . 'Case' . DS . 'Controller' . DS . 'Component');
@@ -11,12 +12,11 @@ class AllTest extends CakeTestSuite {
 		$suite->addTestDirectory(TESTS . 'Case' . DS . 'View' . DS . 'Helper');
 
 		//lib/Nc以下のテストを実行する
-		$suite->addTestDirectory(NC . 'Test/Case' . DS . 'Controller');
-		$suite->addTestDirectory(NC . 'Test/Case' . DS . 'Controller' . DS . 'Component');
-		$suite->addTestDirectory(NC . 'Test/Case' . DS . 'Model');
-		$suite->addTestDirectory(NC . 'Test/Case' . DS . 'Model' . DS . 'Behavior');
-		$suite->addTestDirectory(NC . 'Test/Case' . DS . 'View');
-		$suite->addTestDirectory(NC . 'Test/Case' . DS . 'View' . DS . 'Helper');
+		$suite->addTestDirectory(NC . 'Test' . DS , 'Case' . DS . 'Controller' . DS . 'Component');
+		$suite->addTestDirectory(NC . 'Test' . DS , 'Case' . DS . 'Model');
+		$suite->addTestDirectory(NC . 'Test' . DS , 'Case' . DS . 'Model' . DS . 'Behavior');
+		$suite->addTestDirectory(NC . 'Test' . DS , 'Case' . DS . 'View');
+		$suite->addTestDirectory(NC . 'Test' . DS , 'Case' . DS . 'View' . DS . 'Helper');
 
 		return $suite;
 	}
