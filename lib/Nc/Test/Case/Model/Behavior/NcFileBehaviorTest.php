6a9447f0634aaf84f6eeaf6ef2effd4b99a1cd09
NEKOGET <info@nekoget.net>
2013-10-31 11:11:32 +0900

    deleteFile() option省略分のテストを追加 GetCurrentDir()のテストを追加

--- lib/Nc/Test/Case/Model/Behavior/NcFileBehaviorTest.php
+++ lib/Nc/Test/Case/Model/Behavior/NcFileBehaviorTest.php
@@ -199,6 +199,34 @@ class NcFileBehaviorTest extends CakeTestCase {
 		$this->assertEqual(false , file_exists($file_path));
 		$this->assertEqual(false , file_exists($gz_path));
 
+		//option省略　gzファイルも同時に消す --
+		//ファイル作成
+		$ck      = $this->File->createFile($model , $path , $name , $content ,true);
+		$this->assertEqual($name , $ck); //作成に成功したのでファイル名が戻ってくる
+		$this->assertEqual(true , file_exists($path . DS . $name));//生成したファイルが存在する。
+		$this->assertEqual(true , file_exists($path . DS . $name .  ".gz"));//生成したgzファイルが存在する。
+		$text = file_get_contents($path . DS . $name);
+		$this->assertEqual($text , $content); //ファイルの中にかかれているものが指定したモノ。
+		//ファイルが書き込み可能かしらべる
+		$this->assertEqual(true , is_readable($path . DS .  $name));
+		// ファイル削除 option省略・
+		$ck = $this->File->deleteFile($model , $file_path);
+		//trueが戻る。
+		$this->assertEqual(true , $ck);
+		//ファイルは削除されたので、存在しないはず
+		$this->assertEqual(false , file_exists($file_path));
+		$this->assertEqual(false , file_exists($gz_path));
+		//テストのため生成したファイルを削除
+		$this->unlinkFile($file_path);
+
+		//存在しないファイルを指定して削除。--
+		$ck = $this->File->deleteFile($model , $file_path, true);
+		//存在しないファイルが指定されたのでfalse
+		$this->assertEqual(false , $ck);
+		//ファイルは存在しない
+		$this->assertEqual(false , file_exists($file_path));
+		$this->assertEqual(false , file_exists($gz_path));
+
 		//指定したファイルのみ削除 --
 		$model     = new User;
 		$path      = TMP . "tests";
@@ -262,6 +290,77 @@ class NcFileBehaviorTest extends CakeTestCase {
  * @return void
  */
 	public function testGetCurrentDir() {
+		$model = new User;
+		$path = TMP . "tests" . DS . 'NcFileBehaviorTestDir' . DS;
+		if(! is_dir($path)) mkdir($path , 0777);
+
+		//フォルダの中をしらべる。フォルダは０件。array()が戻る
+		$ck = $this->File->getCurrentDir($model, $path);
+		$this->assertEqual(array() , $ck);
+		//テストで使用したフォルダを削除
+		if(is_dir($path)) rmdir($path);
+		//存在しないフォルダに対して事項する。falseが戻る。
+		$ck = $this->File->getCurrentDir($model, $path);
+		$this->assertEqual(false , $ck);
+
+		$dirArray = array('test1' , 'test2' , 'test3');
+
+		$this->assertEqual( true , mkdir($path , 0777));
+		foreach($dirArray as $i)
+		{
+			if(! is_dir($path . DS . $i)) $this->assertEqual( true , mkdir($path . DS . $i , 0777));
+		}
+		//設置したフォルダと同じものが情報として戻ってきた。
+		$ck = $this->File->getCurrentDir($model, $path);
+		$this->assertEqual(sort($dirArray), sort($ck));
+
+		//ファイルと混在していた場合--
+		$name = "testFile.txt";
+		$content = "unitTest TEXT";
+		$ck = $this->File->createFile($model , $path , $name , $content , true);
+
+		$this->assertEqual($name , $ck); //作成に成功した
+		$this->assertEqual(true , file_exists($path . $name));//生成したファイルが存在する。
+		$this->assertEqual(true , file_exists($path . $name .  ".gz"));//生成したgzファイルが存在する。
+		$text = file_get_contents($path . $name);
+		$this->assertEqual($text , $content); //ファイルの中にかかれているものが指定したモノ。
+		//ファイルが書き込み可能かしらべる
+		$this->assertEqual(true , is_readable($path .  $name));
+		$this->assertEqual(true , is_readable($path . $name . '.gz'));
+
+		//ファイルが混在している状態になった。
+		// フォルダ test1, test2, test3 ファイル testFile.txt testFile.txt.gz
+
+		$ck = $this->File->getCurrentDir($model, $path);
+		$this->assertEqual(sort($dirArray), sort($ck));
+
+		//optionをfalse指定。
+		$ck = $this->File->getCurrentDir($model, $path , false);
+		$this->assertEqual(sort($dirArray), sort($ck));
+
+		//第三引数をtrue指定 ファイルだけの一覧がとれる。
+		$ck = $this->File->getCurrentDir($model, $path ,true);
+		$fileArray =  array($name , $name . '.gz');
+		$this->assertEqual(sort($fileArray), sort($ck));
+
+
+		//テストのため生成したファイルを削除
+		$this->unlinkFile(realpath($path .  $name));
+
+
+		//テストで使用したフォルダを削除
+		foreach($dirArray as $i)
+		{
+			if(is_dir($path . DS . $i)) $this->assertEqual( true , rmdir($path . DS . $i));
+		}
+		if(is_dir($path)) rmdir($path);
+
+		//$is_file = trueの場合
+
+
+
+
+
 	}
 
 /**
