6d4d401e67f0432283bdedddf6c722f8ab844064
NEKOGET <info@nekoget.net>
2013-11-20 10:09:22 +0900

    findRoomList() , createDefaultEntry()のテスト追加 createDefaultEntry()について、一度作成されたら再度実行はされてはいけない機能 後ほど対応予定。

--- lib/Nc/Test/Case/Model/NcPageTest.php
+++ lib/Nc/Test/Case/Model/NcPageTest.php
@@ -15,8 +15,12 @@ class NcPageTest extends CakeTestCase {
  */
 	public $fixtures = array(
 		'NcPage',
-		'NcPageTree'
+		'NcAuthority',
+		'NcPageTree',
+		'NcPageUserLink',
+		'NcUser'
 	);
+
 
 /**
  * setUp method
@@ -26,6 +30,11 @@ class NcPageTest extends CakeTestCase {
 	public function setUp() {
 		parent::setUp();
 		$this->Page = ClassRegistry::init('Page');
+		$this->Page->useDbConfig = 'test';
+		//moderl内で呼ばれているModelの接続先情報の選択設定
+		$this->Page->Authority->useDbConfig    = 'test';
+		$this->Page->PageTree->useDbConfig     = 'test';
+		$this->Page->PageUserLink->useDbConfig = 'test';
 
 
 	}
@@ -189,6 +198,135 @@ class NcPageTest extends CakeTestCase {
  * @return void
  */
 	public function testFindRoomList() {
+		$result = array (
+			9 =>
+			array (
+				'Page' =>
+				array (
+					'id' => '9',
+					'root_id' => '9',
+					'thread_num' => '1',
+					'display_sequence' => '0',
+					'page_name' => 'Public room',
+					'permalink' => '',
+					'position_flag' => '1',
+					'lang' => '',
+					'is_page_meta_node' => '0',
+					'is_page_style_node' => '0',
+					'is_page_layout_node' => '0',
+					'is_page_theme_node' => '0',
+					'is_page_column_node' => '0',
+					'room_id' => '9',
+					'space_type' => '1',
+					'show_count' => '0',
+					'display_flag' => '1',
+					'display_from_date' => NULL,
+					'display_to_date' => NULL,
+					'display_apply_subpage' => '1',
+					'display_reverse_permalink' => NULL,
+					'is_approved' => '1',
+					'lock_authority_id' => '0',
+					'created' => NULL,
+					'created_user_id' => '0',
+					'created_user_name' => '',
+					'modified' => NULL,
+					'modified_user_id' => '1',
+					'modified_user_name' => '',
+				),
+				'PageUserLink' =>
+				array (
+					'authority_id' => NULL,
+				),
+				'PageAuthority' =>
+				array (
+					'id' => NULL,
+					'myportal_use_flag' => NULL,
+					'private_use_flag' => NULL,
+					'hierarchy' => NULL,
+				),
+				'Community' =>
+				array (
+					'publication_range_flag' => NULL,
+					'participate_force_all_users' => NULL,
+					'participate_flag' => NULL,
+					'is_upload' => NULL,
+					'photo' => NULL,
+				),
+				'CommunityLang' =>
+				array (
+					'community_name' => NULL,
+					'summary' => NULL,
+				),
+			),
+			11 =>
+			array (
+				'Page' =>
+				array (
+					'id' => '11',
+					'root_id' => '11',
+					'thread_num' => '1',
+					'display_sequence' => '0',
+					'page_name' => 'Private room of admin',
+					'permalink' => 'Admin',
+					'position_flag' => '1',
+					'lang' => '',
+					'is_page_meta_node' => '0',
+					'is_page_style_node' => '0',
+					'is_page_layout_node' => '0',
+					'is_page_theme_node' => '0',
+					'is_page_column_node' => '0',
+					'room_id' => '11',
+					'space_type' => '3',
+					'show_count' => '0',
+					'display_flag' => '1',
+					'display_from_date' => NULL,
+					'display_to_date' => NULL,
+					'display_apply_subpage' => '1',
+					'display_reverse_permalink' => NULL,
+					'is_approved' => '1',
+					'lock_authority_id' => '0',
+					'created' => NULL,
+					'created_user_id' => '0',
+					'created_user_name' => '',
+					'modified' => NULL,
+					'modified_user_id' => '1',
+					'modified_user_name' => '',
+				),
+				'PageUserLink' =>
+				array (
+					'authority_id' => NULL,
+				),
+				'PageAuthority' =>
+				array (
+					'id' => NULL,
+					'myportal_use_flag' => NULL,
+					'private_use_flag' => NULL,
+					'hierarchy' => NULL,
+				),
+				'Community' =>
+				array (
+					'publication_range_flag' => NULL,
+					'participate_force_all_users' => NULL,
+					'participate_flag' => NULL,
+					'is_upload' => NULL,
+					'photo' => NULL,
+				),
+				'CommunityLang' =>
+				array (
+					'community_name' => NULL,
+					'summary' => NULL,
+				),
+			),
+		);
+
+		//全件
+		$ck = $this->Page->findRoomList('all');
+		$this->assertEqual($result , $ck);
+
+		//id指定
+		$ck = $this->Page->findRoomList(1);
+		var_export($ck);
+
 	}
 
 /**
@@ -301,6 +439,60 @@ class NcPageTest extends CakeTestCase {
  * @return void
  */
 	public function testCreateDefaultEntry() {
+
+		//マイポータル作成, マイルーム作成, ルーム参加
+		$user = array();
+		$user['User'] = array(
+			'id'=>"7",
+			'login_id'=>'admin_7' ,
+			'password'=>'1a9faaae0428d9f50245c1fb77cad74a25fd917a' ,
+			'handle'=>'admin',
+			'authority_id'=>"2",
+			'is_active'=>"1",
+			'permalink'=>'admin_7',
+			'myportal_page_id'=>"10",
+			'private_page_id'=>"11",
+			'avatar'=>NULL,
+			'activate_key'=>NULL,
+			'lang'=>'ja',
+			'timezone_offset'=>"9",
+			'email'=>'',
+			'mobile_email'=>'',
+			'password_regist'=>NULL ,
+			'last_login'=>'2013-09-27 00:42:44',
+			'previous_login'=>'2013-07-22 08:20:15',
+			'created'=>'2013-10-31 04:09:21',
+			'created_user_id'=>"1",
+			'created_user_name'=>'admin',
+			'modified'=>'2013-10-31 09:52:46',
+			'modified_user_id'=>'0',
+			'modified_user_name'=>''
+		);
+
+		//return array($myportalPageId, $privatePageId);
+		//生成された Page.idの値。
+		$result = array (
+			0 => '17',
+			1 => '19',
+		);
+		$ck = $this->Page->createDefaultEntry($user);
+		$this->assertEqual($result , $ck);
+
+		//同じユーザの情報で再度実行しても作られる
+		//1アカウントに対し1回のみの実行となるようにする必要がある。
+		//とりあえず複数件登録されてしまうテストはコメントアウト
+		/*
+		$result = array (
+			0 => '21',
+			1 => '23',
+		);
+		$ck = $this->Page->createDefaultEntry($user);
+		$this->assertEqual($result , $ck);
+		$this->Page->find('list' , array());
+		*/
+
+
+
 	}
 
 /**
