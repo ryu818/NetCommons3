ca011fbe297d81e30b896a3e6044050c834adf59
NEKOGET <info@nekoget.net>
2013-11-20 13:44:10 +0900

    Model/PageのためにFixtureを追加

---
+++ lib/Nc/Test/Fixture/NcPageUserLinkFixture.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *UserLink
+ */
+
+class NcPageUserLinkFixture extends CakeTestFixture {
+
+	public $useDbConfig = 'test';
+	public $name = 'PageUserLink';
+
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'room_id' => array('type' => 'integer', 'null' => false, 'default' => '0', 'key' => 'index'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'authority_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'created_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'created_user_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'modified_user_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'room_id' => array('column' => array('room_id', 'user_id'), 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+
+}
