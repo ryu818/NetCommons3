cdb6e2e4d9f947a59c5f6a0a29d4889d2b98d261
NEKOGET <info@nekoget.net>
2013-12-12 14:56:23 +0900

    Model/PageTree作成 カラム名を一部変更 child_id

--- lib/Nc/Model/PageTree.php
+++ lib/Nc/Model/PageTree.php
@@ -4,6 +4,8 @@
  *
  * <pre>
  *  ページの階層情報を制御する
+ *  このmodelはModel/Pageもしくはそれに類するModel Classから呼び出されて使われることを想定しているため
+ *  このClass内ではトランザクション処理は行なっていません。
  * </pre>
  *
  * @copyright     Copyright 2012, NetCommons Project
@@ -29,12 +31,286 @@ class PageTree extends AppModel
 
 	}
 
+	/**
+	 * 新規ページの追加時に必要なinsert処理
+	 * @param   $page_id
+	 * @param   $parent_id
+	 * @return  bool
+	 * @since   v 3.0.0.0
+	 */
+	public function addPage($pageId , $parentId) {
+		//型チェック
+		if(! is_numeric($pageId) || ! is_numeric($parentId)) {
+			return false;
+		}
 
-	function hoge()
-	{
-		//var_dump($this->ds);
-		//var_dump($this->useDbConfig);
-		return $this->find('all');
+		//先祖一覧
+		$treeList = $this->getParentAll($parentId);
+		$ins  = $this->_createAddArray($pageId , $treeList);
+		//先祖なし...はありえない。必ずspace_type別の、meta 真祖の子供になる。
+		if(! $ins) {
+			//保存すべき情報がない。
+			return false;
+		}
+
+		//このメソッドを使うPageの追加処理のところでトランザクション処理必要。
+		//insert処理
+		foreach($ins as $item) {
+			$this->create();
+			if(! $this->save($item)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
+	/**
+	 * ページを新規登録するときに、insertする用の配列を作る。
+	 * @param   $page_id
+	 * @param   $list
+	 * @return  array
+	 * @since   v 3.0.0.0
+	 */
+	private  function _createAddArray($pageId , $treeList) {
+		//pageが数字じゃないor $listが配列じゃない。
+		if(! is_numeric($pageId)
+			|| ! is_array($treeList)
+			|| ! $treeList //空 1件もない状態はない。
+		) {
+			return array();
+		}
+
+		$ins = array();
+		$con = 0;
+		//親からみた情報をつくる。
+		foreach($treeList as $item) {
+			$ins[$con]['parent_id']   = $item['PageTree']['parent_id'] ;
+			$ins[$con]['child_id'] = $pageId;
+			$ins[$con]['stratum_num'] = $item['PageTree']['stratum_num'] + 1;
+			$con ++;
+		}
+		//自身が先祖のレコードを追加する
+		$con ++;
+		$ins[$con]['parent_id']   = $pageId;
+		$ins[$con]['child_id'] = $pageId;
+		$ins[$con]['stratum_num'] = 0;
+
+		return $ins;
+	}
+
+	/**
+	 * ページの削除
+	 *
+	 * <pre>
+	 * トランザクションはこれを利用するModel内で実装されるべきなのでここではトランザクションをしていません。
+	 * 成功した場合はtrue,失敗した場合はfalseが戻ります。
+	 * </pre>
+	 * @param   int $pageId
+	 * @return  bool
+	 * @since   v 3.0.0.0
+	 */
+	public function deletePage($pageId) {
+		//パラメータチェック
+		if(! is_numeric($pageId) || ! $pageId) {
+			return false;
+		}
+
+		//すぐ上の親を調べる
+		$parentId= $this->getParentOneId($pageId);
+		//直親のいない子は削除できない。メタデータだから。
+		if(! $parentId) return false;
+
+		//子のid情報を取得
+		$childList = $this->_childIdsArray($this->getChildAll($pageId));
+		//移動するノード以下の親子情報を取得
+		$treeList = $this->find('all',
+			array(
+				'conditions'=>array(
+					'PageTree.child_id' => $childList,
+					'PageTree.stratum_num = ' . 1,
+					'PageTree.child_id != '   . $pageId
+				),
+				'order'=>array('PageTree.stratum_num ASC')
+			)
+		);
+
+		//再構築のため、一旦ノードごと削除する
+		if(! $this->deleteNode($pageId)) return false;
+
+		//自身が親になっている場合のみ、$parent_idに差し替えて再構築する。
+		//再構築
+		foreach($treeList as $item)
+		{
+			if(isset($item['PageTree'])
+				&& isset($item['PageTree']['child_id'])
+				&& isset($item['PageTree']['parent_id']) ) {
+				//parent_idがPageIdだった場合は、親の付け替えをする。
+				if($item['PageTree']['parent_id'] == $pageId) {
+					$item['PageTree']['parent_id'] = $parentId;
+				}
+				$ck = $this->addPage($item['PageTree']['child_id'],$item['PageTree']['parent_id']);
+				if(! $ck) return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * 直近の親のidを取得する
+	 * @param   int $pageId
+	 * @return  null or int
+	 * @since   v 3.0.0.0
+	 */
+	private function getParentOneId($pageId) {
+		//パラメータチェック
+		if(! is_numeric($pageId) || ! $pageId) {
+			return null;
+		}
+		$treeList = $this->find(
+			'list',
+			array(
+				'conditions'=>array(
+					'PageTree.child_id'=>$pageId,
+					'PageTree.stratum_num'=>1,
+				),
+				'fields'=>array(
+					'PageTree.parent_id'
+				)
+			)
+		);
+		$ids = array_values($treeList);
+		if(isset($ids[0])) return $ids[0];
+		return null;
+	}
+
+	/**
+	 * pageTreeの一覧（配列）から、子idだけを抜き出した配列を返す
+	 * @param   array $pageTree
+	 * @return  array
+	 * @since   v 3.0.0.0
+	 */
+	private  function _childIdsArray($pageTree) {
+		//パラメータのチェック
+		if(! $pageTree || ! is_array($pageTree) ) {
+			 return array();
+		}
+		$result = array();
+		foreach($pageTree as $item) {
+			 if(isset($item['PageTree']) && isset($item['PageTree']['child_id'])) {
+				 $result[] = $item['PageTree']['child_id'];
+			}
+		}
+		return $result;
+	}
+
+	/**
+	 * ノードごと削除（子孫も一緒に削除）
+	 * @param   int $pageId
+	 * @return  bool
+	 * @since   v 3.0.0.0
+	 */
+	public function deleteNode($pageId) {
+		//パラメータチェック
+		if(! is_numeric($pageId) || ! $pageId) {
+			return false;
+		}
+		//自身も含む子のidの情報が削除できればよい
+		$pageIds = $this->_childIdsArray($this->getChildAll($pageId));
+		$ck = $this->deleteAll(array('PageTree.child_id'=>$pageIds));
+		if(! $ck) return false;
+
+		//無事完了
+		return true;
+	}
+
+	/**
+	 * ページのノードの移動
+	 *
+	 * @param   int $pageId
+	 * @param   int $newParentId
+	 * @return  bool
+	 * @since   v 3.0.0.0
+	 */
+	public function moveNode($pageId , $newParentId) {
+
+		//子のid情報を取得
+		$childList = $this->_childIdsArray($this->getChildAll($pageId));
+		//移動するノード以下の親子情報を取得
+		$treeList = $this->find('all',
+			array(
+				'conditions'=>array(
+						'PageTree.child_id' => $childList,
+						'PageTree.stratum_num = ' . 1,
+						'PageTree.child_id != '   . $pageId
+					),
+					'order'=>array('PageTree.stratum_num ASC')
+				)
+			);
+		//再構築のため、一旦ノードを削除する
+		if(! $this->deleteNode($pageId)) return false;
+
+		//新しい親に追加
+		if(! $this->addPage($pageId , $newParentId)) return false;
+
+		//再構築
+		foreach($treeList as $item)
+		{
+			if(isset($item['PageTree'])
+				&& isset($item['PageTree']['child_id'])
+				&& isset($item['PageTree']['parent_id']) ) {
+					$ck = $this->addPage($item['PageTree']['child_id'],$item['PageTree']['parent_id']);
+					if(! $ck) return false;
+			}
+		}
+		//すべて無事完了
+		return true;
+	}
+
+	/**
+	 * 祖先情報の取得
+	 * 自身のデータまで取得する。
+	 * @param   int$page_id
+	 * @return  array
+	 * @since   v 3.0.0.0
+	 */
+	public function getParentAll($pageId) {
+		//パラメータチェック
+		if(! is_numeric($pageId) || ! $pageId) {
+			return array();
+		}
+		//指定されたidが子になる一覧
+		$ck = $this->find(
+			'all' ,
+			array('conditions'=>array('child_id'=>$pageId))
+		);
+
+		return $ck;
+	}
+
+	/**
+	 *  子孫一覧（全件）
+	 * @param   int$pageId
+	 * @return  array
+	 * @since   v 3.0.0.0
+	 */
+	public function getChildAll($pageId) {
+		//パラメータチェック
+		if(! $pageId || ! is_numeric($pageId)) {
+			 return array();
+		 }
+
+		$ck = $this->find('all' ,
+			array(
+				'conditions'=>array(
+					'PageTree.parent_id'=>$pageId,
+				),
+				'order'=>array(
+					'PageTree.stratum_num ASC' //改装の浅い順
+				)
+			));
+
+		return $ck;
+	}
 }
--- lib/Nc/Test/Case/Model/NcPageTreeTest.php
+++ lib/Nc/Test/Case/Model/NcPageTreeTest.php
@@ -33,10 +33,305 @@ class NcPageTreeTest extends CakeTestCase {
 		parent::tearDown();
 	}
 
-	public function testHoge()
+	public function testGetParent()
 	{
-		$this->assertEqual(true , true);
+		$ck = $this->PageTree->getParentAll(12);
+		$ans = array (
+			0 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '15',
+					'parent_id' => '9',
+					'child_id' => '12',
+					'stratum_num' => '2',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+			1 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '16',
+					'parent_id' => '1',
+					'child_id' => '12',
+					'stratum_num' => '1',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+			2 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '17',
+					'parent_id' => '12',
+					'child_id' => '12',
+					'stratum_num' => '0',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+		);
+		$this->assertEqual($ck , $ans);
+		//存在しないレコード
+		$ck = $this->PageTree->getParentAll(99999999);
+		$ans = array();
+		$this->assertEqual($ck , $ans);
+		//子孫のいないページ  //自身のレコードだけがとれる。 stratum_num:0のデータ
+		$ck = $this->PageTree->getParentAll(1);
+		$ans = array (
+			0 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '1',
+					'parent_id' => '1',
+					'child_id' => '1',
+					'stratum_num' => '0',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+		);
+		$this->assertEqual($ans , $ck);
 	}
 
+	//ページの追加
+	public function testAddPage() {
+		$count = $this->PageTree->find('count');
+		$ck = $this->PageTree->addPage(20 , 12);
+		$this->assertEqual($ck , true);
+		//レコードが増えてる。
+		$this->assertNotEqual($count , $this->PageTree->find('count')) ;
+		$this->assertEqual(true , ($count < $this->PageTree->find('count')));
+		//12の1番めの子孫が20のレコードが存在する。
+		$ck = $this->PageTree->find(
+			'all' ,
+			array('conditions'=>array(
+				'PageTree.parent_id'=>12,
+				'PageTree.child_id'=>20 )
+			)
+		);
+		//1件
+		$this->assertEqual(1 , count($ck));
+	}
+
+	public function testGetChild()
+	{
+		//1の子孫　publicページ
+		$ck = $this->PageTree->getChildAll(1);
+		$ans = array (
+			0 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '1',
+					'parent_id' => '1',
+					'child_id' => '1',
+					'stratum_num' => '0',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+			1 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '9',
+					'parent_id' => '1',
+					'child_id' => '9',
+					'stratum_num' => '1',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+			2 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '16',
+					'parent_id' => '1',
+					'child_id' => '12',
+					'stratum_num' => '1',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+		);
+		$this->assertEqual($ck , $ans);
+
+		//存在しないページ
+		$ck = $this->PageTree->getChildAll(999999999999);
+		$this->assertEqual($ck , array());
+
+		//文字列
+		$ck = $this->PageTree->getChildAll('AAAAAAAA');
+		$this->assertEqual($ck , array());
+
+		//0
+		$ck = $this->PageTree->getChildAll(0);
+		$this->assertEqual($ck , array());
+
+		//2の子孫
+		$ck = $this->PageTree->getChildAll(2);
+		$ans = array (
+			0 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '2',
+					'parent_id' => '2',
+					'child_id' => '2',
+					'stratum_num' => '0',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+			1 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '11',
+					'parent_id' => '2',
+					'child_id' => '10',
+					'stratum_num' => '1',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+			2 =>
+			array (
+				'PageTree' =>
+				array (
+					'id' => '19',
+					'parent_id' => '2',
+					'child_id' => '13',
+					'stratum_num' => '1',
+					'created' => '2013-11-11 12:00:00',
+					'modified' => '2013-11-11 12:00:00',
+				),
+			),
+		);
+		$this->assertEqual($ck , $ans);
+		//var_export($ck);
+	}
+
+	public function test_idsArray()
+	{
+		//$ck = $this->PageTree->getChildAll(11);
+		//$array = $this->PageTree->_idsArray($ck);
+		//$anc = array (
+		//	0 => '14',
+		//	1 => '21',
+		//	2 => '24',
+		//	3 => '27',
+		//);
+		//$this->assertEqual($array , $anc);
+	}
+
+	public function testDeletePage() {
+		//11を削除
+		$ck = $this->PageTree->deletePage(11);
+		$this->assertEqual(true , $ck);
+
+		//11の親を取得
+		$ck = $this->PageTree->getParentAll(11);
+		$this->assertEqual(array() , $ck);
+
+		//11の子を取得
+		$ck = $this->PageTree->getChildAll(11);
+		$this->assertEqual(array() , $ck);
+	}
+
+	public function testDeleteNode() {
+		$ck = $this->PageTree->getChildAll(11);
+		//var_export($ck);
+		//$ck = $this->PageTree->getChildAll(14);
+		//var_export($ck);
+		$ck = $this->PageTree->deleteNode(11);
+		$this->assertEqual(true , $ck);
+		$ck = $this->PageTree->getChildAll(11);
+		$this->assertEqual(array() , $ck);
+		$ck = $this->PageTree->getChildAll(14);
+		$this->assertEqual(array() , $ck);
+	}
+
+	public function testMoveNode()
+	{
+		//11の子供たちに、子を追加する。
+		$this->PageTree->addPage(200 , 14);
+		$this->PageTree->addPage(210 , 14);
+		$this->PageTree->addPage(220 , 14);
+		$this->PageTree->addPage(300 , 200);
+		$this->PageTree->addPage(310 , 200);
+		$this->PageTree->addPage(320 , 200);
+
+		//親が11で子が220をさがしても見つかる
+		$ck = $this->PageTree->find(
+			'all' ,
+			array(
+				'conditions'=>array(
+					'PageTree.parent_id'=>11,
+					'PageTree.child_id'=>220
+				)
+			)
+		);
+		$this->assertEqual(count($ck), 1);
+
+		//14の先祖は11
+		//14の親11から9に移動する。
+		$ck = $this->PageTree->moveNode( 14 , 9 );
+		$this->assertEqual(true , $ck);
+
+		//親が11で子が9をさがしても見つからない
+		$ck = $this->PageTree->find(
+			'all' ,
+			array(
+				'conditions'=>array(
+					'PageTree.parent_id'=>11,
+					'PageTree.child_id'=>14
+				)
+			)
+		);
+
+		//親が11で子が220をさがしても見つからない
+		$ck = $this->PageTree->find(
+			'all' ,
+			array(
+				'conditions'=>array(
+					'PageTree.parent_id'=>11,
+					'PageTree.child_id'=>220
+				)
+			)
+		);
+
+		//親が9で子が14をさがしても見つかる
+		$ck = $this->PageTree->find(
+			'all' ,
+			array(
+				'conditions'=>array(
+					'PageTree.parent_id'=>9,
+					'PageTree.child_id'=>14
+				)
+			)
+		);
+		$this->assertEqual(1 , count($ck));
+
+		//親が9で子が14をさがしても見つかる
+		$ck = $this->PageTree->find(
+			'all' ,
+			array(
+				'conditions'=>array(
+					'PageTree.parent_id'=>9,
+					'PageTree.child_id'=>220
+				)
+			)
+		);
+		$this->assertEqual(1 , count($ck));
+
+
+	}
 
 }
